20b0e2a7e4c5942522f96b9e5320888ecc56761b
nulano <nulano@nulano.eu>
2021-04-02 15:50:23 +0200

    enable verbose wheel build

--- config.sh
+++ config.sh
@@ -113,7 +113,7 @@ function pip_wheel_cmd {
     if [ -z "$IS_MACOS" ]; then
         CFLAGS="$CFLAGS --std=c99"  # for Raqm
     fi
-    pip wheel $(pip_opts) \
+    pip --verbose wheel $(pip_opts) \
         --global-option build_ext --global-option --enable-raqm \
         --global-option --vendor-raqm --global-option --vendor-fribidi \
         -w $abs_wheelhouse --no-deps .
